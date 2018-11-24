f3bfc8fe514a0040d4d8bb1fbfb12322663fc1bc
HARSH DESAI <harshdesai@HARSHs-iMac.local>
2018-11-24 10:54:26 -0600

    Add prefund account script

---
+++ preFundAccount.sh
@@ -0,0 +1,5 @@
+geth --datadir=./privateDataDir --password ./password.txt account new > account1.txt
+geth --datadir=./privateDataDir --password ./password.txt account new > account2.txt
+geth --datadir=./privateDataDir --password ./password.txt account new > account3.txt
+geth --datadir=./privateDataDir --password ./password.txt account new > account4.txt
+geth --datadir=./privateDataDir --password ./password.txt account new > account5.txt
